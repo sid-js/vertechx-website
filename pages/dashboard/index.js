358f3b99973106f5a6570bb8ebb99ba7e6e53c7b
sid-js <sidhsreejil@gmail.com>
2023-04-03 00:01:45 +0530

    Added basic auth functions and dashboard func

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,42 @@
-
+import { getServerSession } from "next-auth";
 import dynamic from "next/dynamic";
+import prisma from "../../prisma/client"
+import { authOptions } from "../api/auth/[...nextauth]"
 
 const App = dynamic(() => import("../../admin/App"), { ssr: false });
 
 const Dashboard = () => {
-  return <App />;
+  return (
+
+      <App />
+
+  );
 };
 
 export default Dashboard;
+
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps = async (ctx) => {
+  const session = await getServerSession(ctx.req,ctx.res,authOptions) 
+  console.log(session)
+  if(!session){
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+  const data = await prisma.admin.findUnique({
+    where: {
+      userId: session.user.id
+    }
+  })
+  return {
+    props: {
+      authenticated: true,
+    }
+  }
+}
