a107d3b5cebae622c1a82c088554108064ac2b7a
Mohit <mohitsharma20020720@gmail.com>
2023-04-06 21:32:54 +0530

    Changed the links

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -4,40 +4,28 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <nav className=" border-transparent-150 bg-none bg-opacity-20  z-50 sticky top-0 px-14 py-2  ">
-      <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-5">
+      <div className="max-w-screen-3xl flex flex-wrap items-center justify-between mx-auto p-5">
         <Link href="#" className="flex items-center">
-          <img src="logo.svg" className="h-10 w-200 m-200px" alt="Technical glitch" aria-current="page" />
+          <img src="logo.svg" className="h-10 w-200 m-200px border-transparent" alt="Technical glitch" aria-current="page" />
         </Link>
         <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent 
             dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
-              <Link href="/Home" class="relative inline-block px-4 py-2 font-medium group">
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black" aria-current="page"></span>
-                <span class="relative text-black group-hover:text-white">Home</span>
-              </Link>
+              <Link href="#" class="block py-2 pl-5 pr-4 text-xl font-space text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+               md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</Link>
             </li>
             <li>
-              <Link href="/About" class="relative inline-block px-4 py-2 font-medium group">
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">About</span>
-              </Link>
+              <Link href="/About" class="block py-2 pl-5 pr-4 text-xl font-space text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+               md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
             </li>
             <li>
-              <Link href="/Events" class="relative inline-block px-4 py-2 font-medium group">
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">Events</span>
-              </Link>
+              <Link href="/Events" class="block py-2 pl-5 pr-4 text-xl font-space text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+               md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Events</Link>
             </li>
             <li>
-              <Link href="/contact" class="relative inline-block px-4 py-2 font-medium group">
-                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span class="relative text-black group-hover:text-white">Contact</span>
-              </Link>
+              <Link href="/Contacts" class="block py-2 pl-5 pr-4 text-xl font-space  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+               md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
             </li>
 
           </ul>
