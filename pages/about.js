f131afb4112ec4ce351dc060a489fa1826431568
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-04-16 01:16:34 +0530

    Change Image

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,11 @@
-import aboutimg from '../public/gocart.JPG';
+import aboutimg from '../public/gocart.jpg';
 import Image from 'next/image';
 
 const about = () => {
   return (
     <>
       <section className='h-screen snap-start shrink-0 z-10 pt-20'>
-        <div className='flex flex-col items-start justify-center w-full mt-8'>
-
-        </div>
+        <div className='flex flex-col items-start justify-center w-full mt-8'></div>
         <article className='flex items-center justify-center mt-6'>
           <div className='flex flex-col justify-center w-full gap-10 bg-white bg-opacity-5 backdrop-blur-sm border h-full border-gray-700 rounded-lg p-14'>
             <div className='text-left text-white text-7xl font-medium mb-9'>
@@ -17,11 +15,13 @@ const about = () => {
             </div>
             <div className='flex items-center justify-center text-left basis-1/2 text-xl'>
               <span className='text-white'>
-                VertechX is an annual technical fest conducted by MVJ College of Engineering in Bangalore,
-                 India. It is a platform for students and tech enthusiasts from different colleges and universities 
-                 to showcase their technical skills and knowledge. The fest includes a series of competitions, workshops,
-                  and seminars in various fields of technology such as robotics, artificial intelligence, machine learning,
-                 and cybersecurity.
+                VertechX is an annual technical fest conducted by MVJ College of
+                Engineering in Bangalore, India. It is a platform for students
+                and tech enthusiasts from different colleges and universities to
+                showcase their technical skills and knowledge. The fest includes
+                a series of competitions, workshops, and seminars in various
+                fields of technology such as robotics, artificial intelligence,
+                machine learning, and cybersecurity.
               </span>
             </div>
             <div className='basis-1/2'>
