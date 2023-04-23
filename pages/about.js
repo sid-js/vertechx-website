99cf40157441a052f997d6c7e5127b2753065d8b
Mohit <mohitsharma20020720@gmail.com>
2023-04-23 11:33:20 +0530

    Temprorary (about and contact)

--- pages/about.js
+++ pages/about.js
@@ -1,40 +1,28 @@
 import MainLayout from '@/Components/MainLayout';
-import aboutimg from '../public/gocart.jpg';
-import Image from 'next/image';
+import ReactPlayer from 'react-player'
 
-const about = () => {
+const About = () => {
   return (
-    <>
-      <MainLayout>
-        <section className='h-screen snap-start shrink-0 z-10 pt-20'>
-          <div className='flex flex-col items-start justify-center w-full mt-8'></div>
-          <article className='flex items-center justify-center mt-6'>
-            <div className='flex flex-col justify-center w-full gap-10 bg-white bg-opacity-5 backdrop-blur-sm border h-full border-gray-700 rounded-lg p-14'>
-              <div className='text-left text-white text-7xl font-medium mb-9'>
-                <span className='absolute top-0 left-0 mt-10 ml-10'>
-                  What is vertechx?
-                </span>
-              </div>
-              <div className='flex items-center justify-center text-left basis-1/2 text-xl'>
-                <span className='text-white'>
-                  VertechX is an annual technical fest conducted by MVJ College of
-                  Engineering in Bangalore, India. It is a platform for students
-                  and tech enthusiasts from different colleges and universities to
-                  showcase their technical skills and knowledge. The fest includes
-                  a series of competitions, workshops, and seminars in various
-                  fields of technology such as robotics, artificial intelligence,
-                  machine learning, and cybersecurity.
-                </span>
-              </div>
-              <div className='basis-1/2'>
-                <Image src={aboutimg} width={1430} height={400} alt='about' />
-              </div>
-            </div>
-          </article>
-        </section>
-      </MainLayout>
-    </>
+    <MainLayout>
+      <section className="h-screen pt-24">
+        <article className="mx-auto max-w-3xl p-8 bg-gray-800 bg-opacity-70 rounded-lg shadow-lg border-4 border-gradient-to-r from-purple-500 to-pink-500">
+          <ReactPlayer
+            playing={true}
+            loop={true}
+            muted={true}
+            width={'100%'}
+            height={'100%'}
+            className='w-full h-full'
+            url='https://ik.imagekit.io/vertechx/gokart.mp4'
+          />
+          <h1 className="text-5xl font-bold text-white mb-8 underline decoration-gradient-to-r from-purple-500 to-pink-500">What is Vertechx?</h1>
+          <p className="text-xl text-white">
+            The place where it all began, the origin of life, and the unexplored realms. What can we gain by sailing to the moon if we are not able to cross the abyss that separates us from ourselves? This is the most important of all voyages of discovery, and without it, all the rest are not only useless but disastrous. There is a fifth dimension, beyond that which is known to man. It is a dimension as vast as space and as timeless as infinity. It is the middle ground between light and shadow, between science and superstition. Let's unravel these mysteries of the universe that is purely geometric- a beautiful shape twisting around and dancing over space-time.
+            Hold your breaths and get ready to launch yourselves into this abyssal realm and embark upon this expedition at the 11th edition of Vertechx. Mark your calendars! 19th-20th May 2023 it is!
+          </p>
+        </article>
+      </section>
+    </MainLayout>
   );
 };
-
-export default about;
+export default About;
